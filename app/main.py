cc6a6e1ede73949b5e7acb7a7ee56e1c9761f88a
Volodymyr Vinohradov <kram3ko@gmail.com>
2025-02-21 00:12:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,34 +1,106 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Deck:
-    def __init__(self, row, column, is_alive=True):
-        pass
+    row: int
+    column: int
+    is_alive: bool = True
+    marker = "□"
 
 
+@dataclass
 class Ship:
-    def __init__(self, start, end, is_drowned=False):
-        # Create decks and save them to a list `self.decks`
-        pass
+    start: tuple[int, int]
+    end: tuple[int, int]
+    is_drowned: bool = False
 
-    def get_deck(self, row, column):
-        # Find the corresponding deck in the list
-        pass
+    def __post_init__(self) -> None:
+        self.decks = [
+            Deck(row, col)
+            for row in range(self.start[0], self.end[0] + 1)
+            for col in range(self.start[1], self.end[1] + 1)
+        ]
 
-    def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+    def get_deck(self, row: int, column: int) -> Deck | None:
+        for deck in self.decks:
+            if deck.row == row and deck.column == column:
+                return deck
+        return None
+
+    def fire(self, row: int, column: int) -> str:
+        for deck in self.decks:
+            if deck.row == row and deck.column == column:
+                deck.is_alive = False
+                deck.marker = "*"
+
+        if all(not deck.is_alive for deck in self.decks):
+            self.is_drowned = True
+            for deck in self.decks:
+                deck.marker = "x"
+            return "Sunk!"
+        return "Hit!"
 
 
+@dataclass
 class Battleship:
-    def __init__(self, ships):
-        # Create a dict `self.field`.
-        # Its keys are tuples - the coordinates of the non-empty cells,
-        # A value for each cell is a reference to the ship
-        # which is located in it
-        pass
+    ships: list[tuple | Ship]
 
-    def fire(self, location: tuple):
-        # This function should check whether the location
-        # is a key in the `self.field`
-        # If it is, then it should check if this cell is the last alive
-        # in the ship or not.
-        pass
+    def __post_init__(self) -> None:
+        self.ships = [Ship(start, end) for start, end in self.ships]
+        self.field: dict[tuple, Ship] = {
+            (deck.row, deck.column): ship
+            for ship in self.ships
+            for deck in ship.decks
+        }
+        self.battle_field = [["~" for _ in range(10)] for j in range(10)]
+        self._validate_field()
+
+    def _validate_field(self) -> None:
+        assert len(self.ships) == 10, "Ships should be exactly 10"
+        assert sum(1 for ship in self.ships if len(ship.decks) == 1) == 4, (
+            "must be 4 ship with 1 deck"
+        )
+        assert sum(1 for ship in self.ships if len(ship.decks) == 2) == 3, (
+            "must be 3 ship with 2 deck"
+        )
+        assert sum(1 for ship in self.ships if len(ship.decks) == 3) == 2, (
+            "must be 2 ship with 3 deck"
+        )
+        assert sum(1 for ship in self.ships if len(ship.decks) == 4) == 1, (
+            "must be 1 ship with 4 deck"
+        )
+        self._is_ship_near()
+
+    def _is_ship_near(self) -> None:
+        for (row, column), ship in self.field.items():
+            deck = ship.get_deck(row, column)
+            neighbors = [
+                (deck.row - 1, deck.column),
+                (deck.row + 1, deck.column),
+                (deck.row, deck.column - 1),
+                (deck.row, deck.column + 1),
+                (deck.row - 1, deck.column - 1),
+                (deck.row - 1, deck.column + 1),
+                (deck.row + 1, deck.column - 1),
+                (deck.row + 1, deck.column + 1),
+            ]
+            for neighbor in neighbors:
+                if neighbor in self.field and self.field[neighbor] != ship:
+                    raise ValueError("another ship too close")
+
+    def update_field(self) -> None:
+        for ship in self.ships:
+            for deck in ship.decks:
+                self.battle_field[deck.row][deck.column] = deck.marker
+
+    def fire(self, location: tuple) -> str | None:
+        if location not in self.field:
+            return "Miss!"
+
+        for coords, ship in self.field.items():
+            row, column = coords
+            if location == coords:
+                shot = ship.fire(row, column)
+                self.update_field()
+                return shot
